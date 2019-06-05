4256012231dadede4e07ff6351ff0649a41b00ab
nhasidi <nhasidi@gmail.com>
2019-06-05 03:40:32 +0300

    add empty project

---
+++ MapReduceTemplate/MapReduceTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapReduceTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
